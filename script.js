5db6bc1248b846c9d719e495d48434483b6a5796
andreiMDL <andreimoda8@gmail.com>
2024-08-27 19:12:44 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    
+    img.setAttribute("src", "./assets/foto-top.png")
+    
+  } else { 
+    // se tiver normal mdark mode, manter a imagem normal
+    img.setAttribute("src", "./assets/Foto-Andrei-Redonda.png")
+    
+    
+  }
+}
