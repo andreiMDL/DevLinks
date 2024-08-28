0d5d1abb96dfc00c1f9b2cce11b694ac208e4803
andreiMDL <andreimoda8@gmail.com>
2024-08-28 20:11:32 -0300

    edit avatar

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if(html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    
-    img.setAttribute("src", "./assets/foto-top.png")
-    
-  } else { 
+
+    img.setAttribute("src", "./assets/Avatar-LightMode.png")
+  } else {
     // se tiver normal mdark mode, manter a imagem normal
-    img.setAttribute("src", "./assets/Foto-Andrei-Redonda.png")
-    
-    
+    img.setAttribute("src", "./assets/Avatar-DarkMode.png")
   }
 }
